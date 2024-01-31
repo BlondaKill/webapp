1a6ece0d8f268cf54e374d113afa3cde8b075cbb
lauramorales <ma.lauramorales@gmail.com>
2024-01-31 16:03:32 -0300

    Artist

--- App.js
+++ App.js
@@ -1,29 +1,148 @@
-import {View, Text, StyleSheet} from 'react-native'
+import { useState } from 'react'
+import {View, Text, TextInput, Button, StyleSheet, FlatList, Modal} from 'react-native'
+import uuid from 'react-native-uuid';
 
 const App = () => {
-    return (
-      <View style={styles.container}>
 
-        <Text style={styles.text}> Never Give up! </Text>
+  const [modalVisible, setModalVisible] = useState(false);
+  const [idSelected, setIdSelected] = useState("")
+  const [newTask, setNewTask] = useState({
+    title: "",
+    description: "",
+    id:""
+  })
+    
+  const [tasks, setTasks] = useState([])
 
+  const addTask = () =>{
+    setTasks([...tasks, newTask])
+
+    setNewTask({
+      title: "",
+      description: "",
+      id: ""
+    })
+    
+  }
+
+  const onHandlerArtist = (t) =>{
+    const id = uuid.v4()
+    setNewTask({...newTask, title:t, id})
+
+  }
+  
+
+  const onHandlerSong = (t) => {
+    setNewTask({...newTask, description:t})
+
+  }
+
+  const onHandlerAlbum = (t) => {
+    setNewTask({...newTask, description:t})
+
+  }
+
+  const onHandlerYear = (t) => {
+    setNewTask({...newTask, description:t})
+
+  }
+
+  const onHandlerModalDelete = (id) => {
+    setIdSelected(id)
+    setModalVisible(true)
+    
+    
+  }
+
+  const deleteTask = () => {
+    setTasks(tasks.filter(task => task.id != idSelected))
+
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>PLAYLIST</Text>
+
+
+      <View style={styles.inputContainer}>
+        <TextInput value={newTask.title} onChangeText={onHandlerArtist} placeholder = 'Artist' style={styles.input}/>
+        <TextInput value={newTask.description} onChangeText={onHandlerSong} placeholder = 'Song' style={styles.input}/>
+        <TextInput value={newTask.album} onChangeText={onHandlerAlbum} placeholder = 'Album' style={styles.input}/>
+        <TextInput value={newTask.year} onChangeText={onHandlerYear} placeholder = 'Year' style={styles.input}/>
+
+
+
+        <Button color="#3921F5" title = 'ADD' onPress={addTask}/>
       </View>
 
-    )
 
+      <View style={styles.tasksContainer}>
+      <FlatList
+        data={tasks}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+                            <View style={styles.taskCard}>
+                              <Text style={styles.text}> {item.title} </Text>
+                              <Button title= 'DEL' onPress={() => onHandlerModalDelete(item.id)}/>
+                              </View>
+
+    )}  
+    />
+    <Modal
+      visible={modalVisible}    
+    >
+      <View>
+        <Text>Delete?</Text>
+        <Button title='si' onPress={()=> {
+          deleteTask()
+          setModalVisible(false)
+          }}/>
+        <Button title='no' onPress={()=> setModalVisible(false)}/>
+      </View>
+    </Modal>
+    </View>
+
+    </View>
+  )
 }
 
 export default App
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "pink",
-    flex:1,
+  container:{
+    backgroundColor: "#F2E4F6", 
+    flex:1, 
+    paddingTop:30
   },
-text: {
-  color: "orange",
-  textAlign: "center",
-  
-  
-}  
+  inputContainer:{
+    backgroundColor: "#8F2FF5",
+    alignItems: "center",
+    justifyContent: "space-around"
 
+  },
+  input:{
+    width: 250,
+    borderBottomWidth:2,
+    borderColor: "white",
+    margin:10, 
+    paddingVertical:5, 
+    paddingHorizontal:10
+  },
+  tasksContainer:{
+    padding: 10,
+
+  },
+  taskCard:{
+    flexDirection: "row",
+    backgroundColor: "#8F2FF5",
+    padding: 20,
+    marginVertical: 10,
+    alignItems: "center",
+    borderRadius: 5,
+  },
+  text:{
+    width: "70%",
+    color: "white",
+    fontSize: 16,
+  }
 })
